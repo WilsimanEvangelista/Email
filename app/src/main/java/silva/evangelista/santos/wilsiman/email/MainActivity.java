0cec7327dd4415b3717a3df620f663ba7dc44a74
20211TIIMI0281 <wilsiman.evangelista.ifes@gmail.com>
2023-03-24 08:02:57 -0300

    App email com comentários

--- app/src/main/java/silva/evangelista/santos/wilsiman/email/MainActivity.java
+++ app/src/main/java/silva/evangelista/santos/wilsiman/email/MainActivity.java
@@ -33,18 +33,26 @@ public class MainActivity extends AppCompatActivity {
                 EditText etTexto = (EditText) findViewById(R.id.etTexto);
                 String texto = etTexto.getText().toString();
 
+                // Criando uma nova intencao para enviar dados para um app que o usuario desejar
                 Intent i = new Intent(Intent.ACTION_SENDTO);
 
+                // Definindo que queremos enviar para um app que trate de receber e enviar emails
                 i.setData(Uri.parse("mailto:"));
 
+                // Preenchendo intent com os dados que queremos enviar para app externa que vai enviar o email
                 String[] emails = new String[]{email};
+                // O campo extra_email corresponde a uma lista de strings onde cada uma é um emial de destinatario
                 i.putExtra(Intent.EXTRA_EMAIL, emails);
+                // Campo extra_subject é referente ao campo de assunto
                 i.putExtra(Intent.EXTRA_SUBJECT,assunto);
+                // Campo extra_text é referente ao corpo de texto do e-mail.
                 i.putExtra(Intent.EXTRA_TEXT, texto);
 
+                // Abaixo tenta-se executar o intent dentro do try. Usa-se o método Intent.createChoose para aparecer vários apps capazes de enviar mensagem de email
                 try {
                     startActivity(Intent.createChooser(i, "Escolha o APP"));
                 }
+                // Abaixo, caso não haja nenhum app capaz de enviar email, aparecerá uma mensagem de erro
                 catch (ActivityNotFoundException e) {
                     Toast.makeText(MainActivity.this, "Não há nenhum app que posso realizar essa operação", Toast.LENGTH_LONG).show();
                 }
